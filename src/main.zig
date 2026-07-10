f98ed1ca26268a4794c19442b6d6688760b74b6f
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-07-10 17:12:35 +0200

    refactor: main testing function

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,16 @@ pub fn main(init: std.process.Init) !void {
         str.content,
         str.content.len,
     });
+
+    try string_search(str, buffer);
+    try string_replacement(str);
+    try string_splitting(str);
+
+    str.clear();
+    std.debug.print("\tClear: content: {s}, len: {}\n", .{ str.content, str.len() });
+}
+
+fn string_search(str: String, buffer: []const u8) !void {
     std.debug.print("\tContains \"is\": {any}\n", .{str.contains("is")});
     std.debug.print("\tFind \"te\": {any}\n\tRfind \"t\": {any}\n", .{
         str.find("te"),
@@ -25,28 +35,32 @@ pub fn main(init: std.process.Init) !void {
         str.count("i"),
     });
     std.debug.print("\tStartsWith \"Thi\" & \"thi\": {any} & {any}\n\tEndsWith \"ext\" & \"Ext\": {any} & {any}\n", .{
-        str.startsWith("Thi"),
-        str.startsWith("thi"),
-        str.endsWith("ext"),
-        str.endsWith("Ext"),
+        str.starts_with("Thi"),
+        str.starts_with("thi"),
+        str.ends_with("ext"),
+        str.ends_with("Ext"),
     });
+}
+
+fn string_replacement(str: String) !void {
     std.debug.print("\tReplace \"s\" => \"z\": {any} changes made, content: {s}\n", .{
         str.replace("s", "z"),
         str.content,
     });
-    str.toUppercase();
+    str.to_uppercase();
     std.debug.print("\tToUpper: {s}\n", .{str.content});
-    str.toLower();
+    str.to_lowercase();
     std.debug.print("\tToLower: {s}\n", .{str.content});
     const split = try str.split(" ");
     std.debug.print("\tSplit \"{s}\" \" \":\n", .{str.content});
     for (split) |s| {
         std.debug.print("\t\t>>> {s}\n", .{s.content});
     }
-    const split_once = try str.splitOnce(" ");
+}
+
+fn string_splitting(str: String) !void {
+    const split_once = try str.split_once(" ");
     std.debug.print("\tSplitOnce \"{s}\" \" \":\n\t\t>>> {s}\n\t\t>>> {s}\n", .{ str.content, split_once[0].content, split_once[1].content });
-    const rsplit_once = try str.rsplitOnce(" ");
+    const rsplit_once = try str.rsplit_once(" ");
     std.debug.print("\tRsplitOnce \"{s}\" \" \":\n\t\t>>> {s}\n\t\t>>> {s}\n", .{ str.content, rsplit_once[0].content, rsplit_once[1].content });
-    str.clear();
-    std.debug.print("\tClear: content: {s}, len: {}\n", .{ str.content, str.len() });
 }
