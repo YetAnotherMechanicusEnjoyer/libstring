7e5ed3a0671771dcbf13ce19b7b064ee653512e7
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-11 08:15:58 +0200

    fix: is_literal to fix pointers to array

--- src/lib/types.zig
+++ src/lib/types.zig
@@ -4,7 +4,14 @@ const std = @import("std");
 /// Returns true if the type is a u8 array, a u8 slice, or a pointer to a u8 array.
 pub fn is_literal(comptime T: type) bool {
     return switch (@typeInfo(T)) {
-        .pointer => |p| p.child == u8 and (p.size == .slice or (p.size == .one and @typeInfo(p.child) == .array)),
+        .pointer => |p| switch (p.size) {
+            .slice => p.child == u8,
+            .one => switch (@typeInfo(p.child)) {
+                .array => |a| a.child == u8,
+                else => false,
+            },
+            else => false,
+        },
         .array => |a| a.child == u8,
         else => false,
     };
